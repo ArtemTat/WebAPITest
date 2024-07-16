7f585fa036cd9063b411ed6e8f090a408d0fb259
ArtemTat <dondigidon456123@mail.ru>
2024-07-16 21:29:53 +0800

    add class Product

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+
+
+namespace WebAPI.Controllers
+{
+    [Route("/api/[controller]")]
+
+    public class ProductsController : Controller
+    {
+        private static List<Product> products = new List<Product>(new[] {
+            new Product() {Id = 1, Name = "Bike", Price = 10000},
+            new Product() {Id = 2, Name = "Car", Price = 300000},
+            new Product() {Id = 3, Name = "Bus", Price = 500000}
+        });
+
+        [HttpGet]
+        public IEnumerable<Product> Get() => products;
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
