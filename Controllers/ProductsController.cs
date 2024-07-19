003c9919d9fff8f4b8e35dfc6f2fd84559bbc279
ArtemTat <dondigidon456123@mail.ru>
2024-07-19 20:33:10 +0800

    add methods put/delete/post

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -18,6 +18,70 @@ namespace WebAPI.Controllers
         [HttpGet]
         public IEnumerable<Product> Get() => products;
 
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = products.SingleOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            products.Remove(products.SingleOrDefault(p => p.Id == id));
+            return Ok(new { Message = "Delete" });
+        }
+
+        private int NextProductId => products.Count() == 0 ? 1 : products.Max(x => x.Id) + 1;
+
+        [HttpGet("GetNextProductId")]
+
+        public int GetmNextProductId()
+        {
+            return NextProductId;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            product.Id = NextProductId;
+            products.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+
+        [HttpPost("Add Product")]
+        public IActionResult PostBody([FromBody] Product product) =>
+            Post(product);
+
+        [HttpPut]
+        public IActionResult Put(Product product) 
+        {
+            if(!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+            var storedProduct = products.SingleOrDefault(p => p.Id == product.Id);
+            if(storedProduct == null) 
+            {
+                return NotFound();
+            }
+            storedProduct.Name = product.Name;
+            storedProduct.Price = product.Price;
+            return Ok(storedProduct);
+        }
+
+        [HttpPut("UpdateProduct")]
+        public IActionResult PutBody([FromBody] Product product) => Put(product);
         //public IActionResult Index()
         //{
         //    return View();
